76a2969e58d8ad37109742ba8066598784ca1064
Mathieu Henrissat <79750131+Henrissat@users.noreply.github.com>
2024-09-18 17:03:08 +0200

    parametrage cors, userlogge authorisation...

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 
-import datasource from './lib/datasource';
-import { ApolloServer } from '@apollo/server';
-import { buildSchema } from 'type-graphql';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import 'reflect-metadata';
+import express, { Request } from "express";
+import http from "http";
+import cors from "cors";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { buildSchema } from "type-graphql";
+
+import datasource from "./lib/datasource";
+import { getUserFromToken } from "./lib/utilities"; // Fonction pour extraire l'utilisateur du token
 import BouteilleResolver from './resolvers/bouteille.resolver';
 import VinResolver from './resolvers/vin.resolvers';
 import RegionResolver from './resolvers/region.resolver';
@@ -12,22 +19,47 @@ import CasierResolver from './resolvers/casier.resolver';
 import UserResolver from './resolvers/user.resolver';
 
 
-async function main() {
+const corsConfig = {
+  origin: ["http://localhost:3000"], 
+  credentials: true, 
+};
+
+const start = async () => {
+  const app = express();
+  app.use(cors(corsConfig));
+
+  const httpServer = http.createServer(app);
+  const port = process.env.PORT || 8000;
 
   const schema = await buildSchema({
     resolvers: [BouteilleResolver, VinResolver, RegionResolver, CepageResolver, CasierResolver, UserResolver],
-    validate: false
+    validate: false,
   });
+
+  // Instanciation et configuration du serveur Apollo
   const server = new ApolloServer({
     schema,
+    csrfPrevention: true,
+    introspection: true,
+    cache: "bounded",
+    plugins: [
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }), 
+      ApolloServerPluginDrainHttpServer({ httpServer }), 
+    ],
+    context: async ({ req }: { req: Request}) => {
+      const { authorization } = req.headers;
+      let userLogged = await getUserFromToken(authorization); 
+      return { userLogged }; 
+    },
   });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 8000 },
-  });
+  await server.start();
+  server.applyMiddleware({ app, cors: false }); 
+
+  await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
+  console.log(`🚀 Serveur prêt à l'adresse http://localhost:${port}${server.graphqlPath}`);
 
   await datasource.initialize();
-  console.log(`🚀  Server ready at: ${url}`);
-}
+};
 
-main();
+start();
