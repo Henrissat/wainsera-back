e6d4cb0851e88df437266288b5cd69821c2cb77c
Mathieu Henrissat <79750131+Henrissat@users.noreply.github.com>
2024-10-03 10:52:31 +0200

    ajout d'un cors local

--- dist/index.js
+++ dist/index.js
@@ -44,7 +44,7 @@ const cepage_resolver_1 = __importDefault(require("./resolvers/cepage.resolver")
 const casier_resolver_1 = __importDefault(require("./resolvers/casier.resolver"));
 const user_resolver_1 = __importDefault(require("./resolvers/user.resolver"));
 const corsConfig = {
-    origin: ["https://wainsera.netlify.app", "https://wainsera.netlify.app/, http://localhost:3000"],
+    origin: ["https://wainsera.netlify.app", "https://wainsera.netlify.app/, http://localhost:3000, http://localhost:3000/"],
     credentials: true,
 };
 const start = async () => {
